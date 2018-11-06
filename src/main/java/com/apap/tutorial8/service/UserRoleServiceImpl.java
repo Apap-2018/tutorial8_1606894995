2009db74af0797dd1b8ee5d3d49823cb8dff1f32
Cut Syifa Salvira <salviraa3@gmail.com>
2018-11-06 13:25:29 +0700

    latihan

---
+++ src/main/java/com/apap/tutorial8/service/UserRoleServiceImpl.java
@@ -0,0 +1,31 @@
+package com.apap.tutorial8.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.apap.tutorial8.model.UserRoleModel;
+import com.apap.tutorial8.repository.UserRoleDB;
+
+@Service
+public class UserRoleServiceImpl implements UserRoleService{
+
+	
+	@Autowired
+	private UserRoleDB userDb; 
+	
+	@Override
+	public UserRoleModel addUser(UserRoleModel user) {
+		String pass = encrypt(user.getPassword());
+		user.setPassword(pass);
+		return userDb.save(user);
+	}
+
+	@Override
+	public String encrypt(String password) {
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		String hashedPassword = passwordEncoder.encode(password);
+		return hashedPassword;
+	}
+
+}
